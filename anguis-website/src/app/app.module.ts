2dc55dd9f60982babbbfc5fb62dcda98e3236301
DamianBasista <damian.basista@gmail.com>
2017-10-22 12:01:39 +0200

    naszaSiedziba

--- anguis-website/src/app/app.module.ts
+++ anguis-website/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './home_page/header/header.component';
 import { FooterComponent } from './home_page/footer/footer.component';
 import { MenuComponent } from './home_page/menu/menu.component';
+import { BodyComponent } from './home_page/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
